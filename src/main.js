f289185a356ca6499b1d47659af63309ecf09a72
Timofey <whatisflopz@gmail.com>
2024-10-15 13:03:16 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css';
+import '../styles/components/navbar.css';
+import '../styles/components/hero.css';
+import '../styles/utils.css';
+
