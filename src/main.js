fb52d7fcd7682b997f8785cb916f83598cb31b90
Timofey <whatisflopz@gmail.com>
2024-10-15 19:13:02 +0300

    add some sections

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import '../styles/modern-normalize.css'
 import '../styles/style.css';
 import '../styles/components/navbar.css';
 import '../styles/components/hero.css';
+import '../styles/components/info.css';
+import '../styles/components/cash.css';
 import '../styles/utils.css';
-
