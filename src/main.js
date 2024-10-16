3ccb6363bae4f648010e32b85bbee6b136a7d9b9
Timofey <whatisflopz@gmail.com>
2024-10-16 19:43:53 +0300

    fix sections

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css';
+import '../styles/utils.css';
 import '../styles/components/navbar.css';
 import '../styles/components/hero.css';
 import '../styles/components/info.css';
 import '../styles/components/cash.css';
-import '../styles/utils.css';
+
+
