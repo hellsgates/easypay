cac2b22e7411696e09e7c0fea0cd2af10ba92c44
Timofey <whatisflopz@gmail.com>
2024-10-26 20:32:04 +0300

    add download section

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import '../styles/components/navbar.css';
 import '../styles/components/hero.css';
 import '../styles/components/info.css';
 import '../styles/components/cash.css';
+import '../styles/components/download.css';
 
 
